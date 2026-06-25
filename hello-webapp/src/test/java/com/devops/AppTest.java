7aa27a1a31d390b60d67491ccb992f1ea8680c37
angshu-skype <angshumanskype@gmail.com>
2026-06-25 09:49:11 +0000

    add test1

---
+++ hello-webapp/src/test/java/com/devops/AppTest.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
